12d2bc2cdf92ee0da7fbce04dce443d4480e1036
Alex Vong <avong003@gmail.com>
2022-04-04 00:13:18 -0700

    site header php

---
+++ site-header.php
@@ -0,0 +1,13 @@
+<masthead>
+	<nav class="site-menu">
+		<a href="" class="logo"></a>
+		<div class="site-menu-links">
+			<a href="" class="calm-voice">Some Page</a>
+			<a href="" class="calm-voice">Some Other Page</a>
+		</div>
+	</nav>
+
+	<nav class="user-menu">
+		<a href="" class="calm-voice">Sign-In</a>
+	</nav>
+</masthead>
