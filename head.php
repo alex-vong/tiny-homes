7c278dd090959a2d5dcebfc47637dbb657fa41fd
Alex Vong <avong003@gmail.com>
2022-04-04 00:13:04 -0700

    updated head meta tags

---
+++ head.php
@@ -0,0 +1,25 @@
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Buy A House</title>
+	<meta
+		name="description"
+		content="Houses and Stuff"
+	/>
+	<meta
+		name="og:description"
+		content="Super Responsive Realtor Website"
+	/>
+
+	<meta property="og:title" content="Alex Made This" />
+	<meta property="og:type" content="">
+	<meta property="og:url" content="">
+
+	<meta property="og:image" content="https://peprojects.dev/alpha-1/alexvong/images/rlc.png" />
+	<link rel="shortcut icon" type="image/jpg" href="[[favicon]]" />
+
+	<link rel="stylesheet" href="css/styles.css" />
+
+</head>
+
