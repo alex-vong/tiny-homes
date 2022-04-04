ed0857cde0756dfdb5319fd5df57c3494cfc9bf8
Alex Vong <avong003@gmail.com>
2022-04-04 00:13:26 -0700

    cta php

---
+++ call-to-action.php
@@ -0,0 +1,10 @@
+<div class="cta-container">
+	<h3 class="stern-voice">
+		This module is a "call to action".
+	</h3>
+	<p class="calm-voice">
+		These aren't names you should memorize. We're just making them up. Sometimes things get called something so many times, that it sticks. This is a pretty common pattern.
+	</p>
+
+	<button class="calm-voice">here's the action!</button>
+</div>
