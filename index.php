32120cbf3b114d0659ef0147b3f4e74a478cd970
Alex Vong <avong003@gmail.com>
2022-04-04 00:12:51 -0700

    created index php and loaded up php includes

---
+++ index.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<?php include('head.php')?>
+
+<body>
+	<header class="site-header">
+		<inner-column>
+			<?php include('site-header.php')?>
+		</inner-column>
+	</header>
+
+	<section class="landing-section">
+		<inner-column>
+			<?php include('landing-page.php')?>
+		</inner-column>
+	</section>
+
+	<section class="call-to-action cta-blue">
+		<inner-column>
+			<?php include('call-to-action.php')?>
+		</inner-column>
+	</section>
+
+	<section class="article-grid">
+		<inner-column>
+			<?php include('article-grid.php')?>
+		</inner-column>
+	</section>
+
+	<section class="call-to-action cta-red">
+		<inner-column>
+			<?php include('call-to-action.php')?>
+		</inner-column>
+	</section>
+
+	<section class="footer">
+		<inner-column>
+			<section class="footer-container">
+				<div class="footer-text">
+					<h3 class="stout-voice">
+			  			This is the site footer and it has a "site-map" module in it.
+					</h3>
+					<p class="calm-voice">
+						Pretty standard type of thing, right?
+					</p>
+				</div>
+
+				<div class="footer-grid">
+					<section class="footer-heading">
+						<a href="" class="logo"></a>
+						<a href="" class="top">Some Page</a>
+						<a href="">Some Other Page</a>
+					</section>
+
+					<section class="footer-user">
+						<a href="">Sign-In</a>
+					</section>
+					
+					<section class="footer-legal">
+						<a href="">Legal Thing A</a>
+						<a href="">Legal Thing B</a>
+
+					</section>
+					
+					<section class="footer-social">
+						<a href="">Social Thing A</a>
+						<a href="">Social Thing B</a>
+						<a href="">Social Thing B</a>
+						<a href="">Social Thing D</a>
+					</section>
+				</div>
+			</section>
+		</inner-column>
+	</section>
+	
+</body>
+</html>
