d98e581a087560c21b3f7f3a1107a1eebd87cc15
Alex Vong <avong003@gmail.com>
2022-04-17 14:57:00 -0700

    updated langin page video

---
+++ landing-page.php
@@ -0,0 +1,17 @@
+<section class="landing-content">
+<!-- 	<picture>
+		<img src='hero.gif' alt="shipping-container-home">
+	</picture> -->
+	<section class="hero-image">
+		 <video src="houses.mp4" playsinline muted loop autoplay class="video"></video> 
+
+		<div class="overlay"></div>
+
+	</section>
+	<div class="landing-text">
+		<h1 class="loud-voice">Hello! This is a 'diptych'</h1>
+		<p class="calm-voice">
+			You can call it whatever you want. It's a module that has an image and some supporting text. On a smaller screen it works nicely to stack. On a larger screen it fits side by side.
+		</p>
+	</div>
+</section>
