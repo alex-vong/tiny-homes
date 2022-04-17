a215d6478529c879ab81d9166b941511185629e0
Alex Vong <avong003@gmail.com>
2022-04-17 14:57:53 -0700

    added card info for card grid

---
+++ article-grid-info.php
@@ -0,0 +1,46 @@
+<?php
+
+	$gridInfo = [
+		[	
+			"img" => 'images/shipping-home-a.jpg',
+			"head" => "This is an article card",
+			"text" => "It's basically a mini call to action, right?",
+			"button" => "Read More!",
+		],
+
+		[	
+			"img" => 'images/shipping-home-b.jpg',
+			"head" => "This is the title of an article card",
+			"text" => "Here's a little info to help you understand if this is an article you want to read",
+			"button" => "Read More!",
+		],
+
+		[	
+			"img" => 'images/shipping-home-c.jpg',
+			"head" => "This is the title of an article card",
+			"text" => "Here's a little info to help you understand if this is an article you want to read",
+			"button" => "Read More!",
+		],
+
+		[	
+			"img" => 'images/shipping-home-d.jpg',
+			"head" => "This is the title of an article card",
+			"text" => "Here's a little info to help you understand if this is an article you want to read",
+			"button" => "Read More!",
+		],
+
+		[	
+			"img" => 'images/shipping-home-e.jpg',
+			"head" => "This is the title of an article card",
+			"text" => "Here's a little info to help you understand if this is an article you want to read",
+			"button" => "Read More!",
+		],
+
+		[	
+			"img" => 'images/shipping-home-f.jpg',
+			"head" => "This is the title of an article card",
+			"text" => "Here's a little info to help you understand if this is an article you want to read",
+			"button" => "Read More!",
+		],
+
+		];
